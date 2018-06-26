eaff4b115d4a931eaa53039c6ae522fa156b382e
Carl Bolduc <contact@carlbolduc.com>
2018-06-25 22:09:19 -0400

    Add roles

---
+++ app/controllers/roles_controller.rb
@@ -0,0 +1,49 @@
+class RolesController < ApplicationController
+  def index
+    @roles = Role.all
+  end
+
+  def show
+    @role = Role.find(params[:id])
+  end
+
+  def new
+    @role = Role.new
+  end
+
+  def create
+    @role = Role.new(role_params)
+    if @role.save
+      flash[:success] = t('.success')
+      redirect_to @role
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @role = Role.find(params[:id])
+  end
+
+  def update
+    @role = Role.find(params[:id])
+    if @role.update_attributes(role_params)
+      flash[:success] = t('.success')
+      redirect_to @role
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    Role.find(params[:id]).destroy
+    flash[:success] = t('.success')
+    redirect_to roles_url
+  end
+
+  private
+
+  def role_params
+    params.require(:role).permit(:name)
+  end
+end
